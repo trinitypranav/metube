6ae4ec850993487980106685b3e860aa0e6bbc49
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-24 01:56:12 +0530

    adding searchCache slice to the store. It is memoization to the search suggestions.

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import app from "./slices/appSlice";
+import searchCache from "./slices/searchCacheSlice";
 
 const store = configureStore({
   reducer: {
     app: app,
+    searchCache: searchCache,
   },
 });
 
