eee6626e31f3fff35695938a004c344459980d1c
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-24 01:57:23 +0530

    video card for each youtube video

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({ info }) => {
+  const { snippet, statistics } = info;
+  // console.log(info);
+  return (
+    <div className="w-72 p-2 m-2 border rounded-xl shadow-lg">
+      <img alt="video" src={snippet.thumbnails.medium.url} />
+      <h1 className="font-semibold">{snippet.title}</h1>
+      <span className="text-sm">{snippet.channelTitle}</span>
+      <span className="text-xs "> ({statistics.viewCount} views)</span>
+    </div>
+  );
 };
 
 export default VideoCard;
