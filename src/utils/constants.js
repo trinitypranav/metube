3a3225d249efdd3654bebce4f0d3e8b9d9d8a3dc
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-27 23:05:10 +0530

    Added constants file for Keys and APIs

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const REACT_APP_YOUTUBE_API_KEY =
+  "AIzaSyCgHBslS98vBE7EbL1RnnauiqCjE74Kp-8";
+export const REACT_APP_YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=AIzaSyCgHBslS98vBE7EbL1RnnauiqCjE74Kp-8";
+export const REACT_APP_YOUTUBE_SEARCH_SUGGESTIONS_API =
+  "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
