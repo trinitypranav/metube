6936168aaffea6667ed664b602df0319e6fa319e
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-27 23:06:15 +0530

    adding buttons list in the main container

--- src/components/ButtonsList.js
+++ src/components/ButtonsList.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+const buttons = [
+  "All",
+  "Music",
+  "Programming",
+  "Movies",
+  "React",
+  "Comedy",
+  "News",
+];
+
 const ButtonsList = () => {
-  return <div>ButtonsList</div>;
+  return (
+    <div className="p-2 flex flex-wrap border">
+      {buttons.map((btn) => (
+        <button key={btn} className="px-4 py-1 border m-2 rounded-lg">
+          {btn}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default ButtonsList;
