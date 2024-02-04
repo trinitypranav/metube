09db5653f2d45faa100589314579194c017e12be
Pranav Jadhav <trinitypranav@gmail.com>
2024-02-04 22:36:54 +0530

    adding dark slice to store reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import app from "./slices/appSlice";
 import searchCache from "./slices/searchCacheSlice";
+import darkSlice from "./slices/darkSlice";
 
 const store = configureStore({
   reducer: {
     app: app,
     searchCache: searchCache,
+    dark:darkSlice
   },
 });
 
