8bdff7fa7578c0776eb5bec222cd8d4931a2c372
Pranav Jadhav <trinitypranav@gmail.com>
2024-02-15 20:21:10 +0530

    video container implementation

--- src/components/VideosContainer.js
+++ src/components/VideosContainer.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
 
 const VideosContainer = () => {
-  return <div>VideosContainer</div>;
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const response = await fetch(process.env.REACT_APP_YOUTUBE_VIDEOS_API);
+    const data = await response.json();
+    // console.log(data.items);
+    setVideos(data.items);
+  }
+
+  if (videos.length === 0) return null;
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <Link key={video.id} to={"/watch?v=" + video.id}>
+          <VideoCard info={video} />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default VideosContainer;
